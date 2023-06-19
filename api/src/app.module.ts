d8290abc7024ddaeb684fa3f9018361d2d4af17a
Isaac Batista <isaacbatst@gmail.com>
2023-06-18 22:59:09 -0300

    remove unused app components

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AdminModule } from './modules/admin/admin.module';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { AuthModule } from './modules/auth/auth.module';
 import { CoreModule } from './modules/core/core.module';
 import { MusclesModule } from './modules/muscles/muscles.module';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule, MusclesModule, AdminModule, CoreModule],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
