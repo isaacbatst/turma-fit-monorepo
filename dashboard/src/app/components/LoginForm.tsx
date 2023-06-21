3170ea8462b54631f28df6c424ad1ffe2c793e6a
Isaac Batista <isaacbatst@gmail.com>
2023-06-20 23:46:22 -0300

    simplify login layout

--- dashboard/src/app/components/LoginForm.tsx
+++ dashboard/src/app/components/LoginForm.tsx
@@ -4,26 +4,33 @@ type Props = {}
 
 const LoginForm = (props: Props) => {
   return (
-    <form className="py-10 px-24 rounded-2xl shadow-lg flex flex-col gap-5">
-      <div className="mb-1">
-        <h2 className='text-2xl font-medium mb-1'>Dashboard da TurmaFit</h2>
+    <form className="flex flex-col gap-10">
+      <div className=''>
+        <h2 className='text-4xl font-semibold mb-2'>Dashboard TurmaFit</h2>
         <p className="text-sm text-stone-950 opacity-50">Acesse o dashboard da administração</p>
       </div>
-      <div className="flex flex-col">
-        <label className="text-sm mb-1" htmlFor="login-email">E-mail</label>
-        <input type="text" name="login-email" id="login-email" 
-          className='bg-transparent text-stone-950 py-2 px-3 outline-none border-2 border-stone-200 rounded-lg
-                focus:border-stone-600' />
-      </div>
-      <div className="flex flex-col">
-        <label className="text-sm mb-1" htmlFor="login-password">Senha</label>
-        <input type="password" name="login-password" id="login-password" 
-          className='bg-transparent text-stone-950 py-2 px-3 outline-none border-2 border-stone-200 rounded-lg
-              focus:border-stone-600' />
+      <div className='flex flex-col gap-3'>
+        <div className="flex flex-col">
+          <label className="text-sm mb-1 font-semibold" htmlFor="login-email">E-mail</label>
+          <input type="text" name="login-email" id="login-email" 
+            className='bg-transparent text-stone-950 
+          py-2 px-3 outline-none shadow-md rounded-lg
+          focus:border-stone-600' 
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm mb-1 font-semibold" htmlFor="login-password">Senha</label>
+          <input type="password" name="login-password" id="login-password" 
+            className='bg-transparent text-stone-950 
+            py-2 px-3 outline-none shadow-md rounded-lg
+            focus:border-stone-600' />
+        </div>
       </div>
       <button type="submit" 
-        className="bg-stone-900 hover:scale-105 active:scale-110 transition 
-              text-white py-3 px-4 rounded-lg">
+        className="from-yellow-400 to-amber-500 bg-gradient-to-r 
+          hover:scale-105 active:scale-110 transition 
+          text-white font-semibold py-3 px-4 rounded-xl"
+      >
         Entrar
       </button>
     </form>
--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
 import LoginForm from "./components/LoginForm";
 
 export default function Home() {
   return (
     <main className="bg-white text-stone-950">
-      <div className="min-h-screen flex">
-        <div className="flex-1 flex justify-center items-center">
+      <div className="min-h-screen flex flex-col">
+        <div
+          className="py-10 
+          shadow-lg gap-5 bg-white
+          flex-1
+          w-full px-5 
+          flex items-center justify-center
+          "
+        >
           <LoginForm />
-        </div>
-        <div className="hidden lg:block flex-1">
-          <Image className="h-full object-cover" src="/login-bg.jpg" alt="workout" width={1920} height={1080} />
         </div>
       </div>
     </main>
