6d8ac8647495036722a6c30a6a8925bf30185908
Isaac Batista <isaacbatst@gmail.com>
2023-08-16 16:39:40 -0300

    test

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PRISMA_SERVICE } from './constants/tokens';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const prismaService = app.get(PRISMA_SERVICE);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  await app.listen(5555, '0.0.0.0');
 }
 bootstrap();
