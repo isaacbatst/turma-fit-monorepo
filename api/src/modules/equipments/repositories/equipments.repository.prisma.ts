3d887d93b6b60278c8b36756c2c2a5283cd37893
Isaac Batista <isaacbatst@gmail.com>
2023-09-22 00:55:55 -0300

    :recycle: Use prisma repository to adapt errors

--- api/src/modules/equipments/repositories/equipments.repository.prisma.ts
+++ api/src/modules/equipments/repositories/equipments.repository.prisma.ts
@@ -1,55 +1,53 @@
-import { Inject } from '@nestjs/common';
 import { PrismaErrorAdapter } from '../../../common/adapters/prisma-errors.adapter';
-import { PRISMA_SERVICE } from '../../../constants/tokens';
-import { PrismaService } from '../../core/DataSource/prisma.service';
-import { EquipmentsRepository } from './equipments.repository';
+import { RepositoryPrisma } from '../../../common/repositories/repository.prisma';
 import { Equipment } from '../entities/equipment.entity';
 
-export class EquipmentsRepositoryPrisma implements EquipmentsRepository {
-  private readonly prismaErrorAdapter = new PrismaErrorAdapter('equipment');
+export class EquipmentsRepositoryPrisma extends RepositoryPrisma<Equipment> {
+  errorAdapter = new PrismaErrorAdapter('equipment');
 
-  constructor(@Inject(PRISMA_SERVICE) private prisma: PrismaService) {}
-  async create(equipment: Equipment): Promise<void> {
-    try {
-      await this.prisma.equipment.create({
-        data: {
-          name: equipment.name,
-          id: equipment.id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
+  protected async unhandledCreate(item: Equipment): Promise<void> {
+    await this.prisma.equipment.create({
+      data: {
+        name: item.name,
+        id: item.id,
+      },
+    });
   }
-  async findAll(): Promise<Equipment[]> {
+
+  protected async unhandledFindAll(): Promise<Equipment[]> {
     const mucles = await this.prisma.equipment.findMany();
     return mucles.map(
       (equipment) => new Equipment(equipment.id, equipment.name),
     );
   }
-  async update(equipment: Equipment): Promise<void> {
-    try {
-      await this.prisma.equipment.update({
-        data: {
-          name: equipment.name,
-        },
-        where: {
-          id: equipment.id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
+
+  protected async unhandledFindById(
+    id: string,
+  ): Promise<Equipment | undefined> {
+    const equipment = await this.prisma.equipment.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return equipment ? new Equipment(equipment.id, equipment.name) : undefined;
   }
-  async remove(id: string): Promise<void> {
-    try {
-      await this.prisma.equipment.delete({
-        where: {
-          id: id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
+
+  protected async unhandledUpdate(updated: Equipment): Promise<void> {
+    await this.prisma.equipment.update({
+      data: {
+        name: updated.name,
+      },
+      where: {
+        id: updated.id,
+      },
+    });
+  }
+
+  protected async unhandledRemove(id: string): Promise<void> {
+    await this.prisma.equipment.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
--- api/src/modules/muscles/repositories/mucles.repository.prisma.ts
+++ api/src/modules/muscles/repositories/mucles.repository.prisma.ts
@@ -1,60 +1,47 @@
-import { Inject } from '@nestjs/common';
 import { PrismaErrorAdapter } from '../../../common/adapters/prisma-errors.adapter';
-import { PRISMA_SERVICE } from '../../../constants/tokens';
-import { PrismaService } from '../../core/DataSource/prisma.service';
+import { RepositoryPrisma } from '../../../common/repositories/repository.prisma';
 import { Muscle } from '../entities/muscle.entity';
-import { MusclesRepository } from './muscles.repository';
 
-export class MusclesRepositoryPrisma implements MusclesRepository {
-  private readonly prismaErrorAdapter = new PrismaErrorAdapter('muscle');
+export class MusclesRepositoryPrisma extends RepositoryPrisma<Muscle> {
+  readonly errorAdapter = new PrismaErrorAdapter('muscle');
 
-  constructor(@Inject(PRISMA_SERVICE) private prisma: PrismaService) {}
-  async create(muscle: Muscle): Promise<void> {
-    try {
-      await this.prisma.muscle.create({
-        data: {
-          name: muscle.name,
-          id: muscle.id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
+  protected async unhandledCreate(item: Muscle): Promise<void> {
+    await this.prisma.muscle.create({
+      data: {
+        name: item.name,
+        id: item.id,
+      },
+    });
   }
-  async findById(id: string): Promise<Muscle | undefined> {
+
+  protected async unhandledFindAll(): Promise<Muscle[]> {
+    const mucles = await this.prisma.muscle.findMany();
+    return mucles.map((muscle) => new Muscle(muscle.id, muscle.name));
+  }
+
+  protected async unhandledFindById(id: string): Promise<Muscle | undefined> {
     const muscle = await this.prisma.muscle.findUnique({
       where: { id },
     });
     return muscle ? new Muscle(muscle.id, muscle.name) : undefined;
   }
 
-  async findAll(): Promise<Muscle[]> {
-    const mucles = await this.prisma.muscle.findMany();
-    return mucles.map((muscle) => new Muscle(muscle.id, muscle.name));
+  protected async unhandledUpdate(updated: Muscle): Promise<void> {
+    await this.prisma.muscle.update({
+      data: {
+        name: updated.name,
+      },
+      where: {
+        id: updated.id,
+      },
+    });
   }
-  async update(muscle: Muscle): Promise<void> {
-    try {
-      await this.prisma.muscle.update({
-        data: {
-          name: muscle.name,
-        },
-        where: {
-          id: muscle.id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
-  }
-  async remove(id: string): Promise<void> {
-    try {
-      await this.prisma.muscle.delete({
-        where: {
-          id: id,
-        },
-      });
-    } catch (err) {
-      this.prismaErrorAdapter.adapt(err);
-    }
+
+  protected async unhandledRemove(id: string): Promise<void> {
+    await this.prisma.muscle.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
