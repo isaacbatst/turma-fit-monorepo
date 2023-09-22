ac451fb6eaecd1578aceeb30b231699c7d949d1f
Isaac Batista <isaacbatst@gmail.com>
2023-09-21 22:36:18 -0300

    :lipstick: Update header logo

--- dashboard/src/app/components/AppHeader.tsx
+++ dashboard/src/app/components/AppHeader.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 const AppHeader = () => {
   return (
-    <header className='flex justify-between items-center p-5'>
-      <Image 
+    <header className='flex justify-between items-center p-5 md:px-20'>
+      <Link href='/'><Image 
         alt='Logo Turma Fit' 
-        src='/turma-fit/base/icon/base_icon_transparent_background.png' 
-        height={80} width={80} 
-      />
+        src='/turma-fit/base/full/base_logo_transparent_background.png' 
+        height={150} width={150} 
+      /></Link>
       <Link href='/my-account' className='font-semibold hover:scale-105 active:opacity-80'>Minha Conta</Link>
     </header>
   )
