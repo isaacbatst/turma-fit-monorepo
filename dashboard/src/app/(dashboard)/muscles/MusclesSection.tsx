6241681aa7f22ea25019ce4a75de26506b711a48
Isaac Batista <isaacbatst@gmail.com>
2023-09-21 22:36:53 -0300

    :iphone: Make muscles grid responsive

--- dashboard/src/app/(dashboard)/muscles/MusclesSection.tsx
+++ dashboard/src/app/(dashboard)/muscles/MusclesSection.tsx
@@ -6,7 +6,7 @@ const MusclesSection = () => {
   return (
     <section>
       <h2 className="text-center text-3xl font-semibold mb-10">Músculos</h2>
-      <ul className="grid grid-cols-3 gap-3 my-10">
+      <ul className="grid xl:grid-cols-2 2xl:grid-cols-3 gap-3 my-10">
         <CreateMuscleForm />
         <MusclesList />
       </ul>
