4b63e1c648dd4cad2d8b73555687ce0b683d9aea
Isaac Batista <isaacbatst@gmail.com>
2023-09-28 22:26:54 -0300

    :speech_balloon: Update workflows texts

--- dashboard/src/app/(dashboard)/page.tsx
+++ dashboard/src/app/(dashboard)/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Dashboard() {
   return (
     <section className='flex flex-col gap-3 p-5'>
-      <h2 className='font-semibold text-xl mb-5'>Bem vindo, Administrador!</h2>
+      <h2 className='font-semibold text-xl mb-5'>Bem vindo!</h2>
       <Link href='/trainings'>
         <Card className="hover:border-slate-500 flex justify-center">
           <p>Ver meus treinos</p>
