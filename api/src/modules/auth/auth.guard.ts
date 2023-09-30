6ea8048258fef3da679dd054fdef61b525269012
Isaac Batista <isaacbatst@gmail.com>
2023-09-30 15:14:23 -0300

    :loud_sound: Add token logs

--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -26,6 +26,7 @@ export class AuthGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromRequest(request);
+    console.log('token', token);
     if (!token) {
       throw new UnauthorizedException('MISSING_TOKEN');
     }
